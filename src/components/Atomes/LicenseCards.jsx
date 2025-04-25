587181f027f24cb3101f4704c4a1c1fb8b0f2511
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2025-04-25 22:37:06 +0530

    Added OverallReport

--- src/components/Atomes/LicenseCards.jsx
+++ src/components/Atomes/LicenseCards.jsx
@@ -1,5 +1,5 @@
-import { Card, CardContent } from "@/components/ui/card"
-import { Lock } from "lucide-react"
+import { Card, CardContent } from "@/components/ui/card";
+import { NotebookPen } from "lucide-react";
 
 const licenses = [
   {
@@ -32,15 +32,17 @@ const licenses = [
     expiry: "24-09-2025",
     status: "Active",
   },
-]
+];
 
 export default function LicenseCards() {
   return (
     <div className="p-4">
-       <button className="text-sm text-blue-600 hover:underline bottom-2 bg-gray-200 rounded-lg p-1">← Go Esck</button>
-        
+      <button className="text-sm text-blue-600 hover:underline bottom-2 bg-gray-200 rounded-lg p-1">
+        ← Go Esck
+      </button>
+
       <div className="flex items-center mt-5 mb-8 gap-4">
-      <h1 className="text-sm font-semibold">Dashboard</h1>
+        <h1 className="text-sm font-semibold">Dashboard</h1>
         <h1 className="text-sm font-semibold">Autodialer</h1>
       </div>
 
@@ -48,7 +50,7 @@ export default function LicenseCards() {
         {licenses.map((license, index) => (
           <Card key={index} className="bg-green-50 text-sm p-2">
             <CardContent className="flex flex-col gap-1">
-              <Lock className="h-4 w-4 mb-1 text-gray-600" />
+              <NotebookPen className="h-4 w-4 mb-1 text-gray-600" />
               <h2 className="font-semibold">{license.title}</h2>
               <p>License Count: {license.count}</p>
               <p>Expiry: {license.expiry}</p>
@@ -58,5 +60,5 @@ export default function LicenseCards() {
         ))}
       </div>
     </div>
-  )
+  );
 }
--- src/components/Atomes/OverallReport.jsx
+++ src/components/Atomes/OverallReport.jsx
@@ -1,10 +1,141 @@
+import {
+  DollarSign,
+  Euro,
+  Info,
+  MessageCircle,
+  PhoneMissed,
+  PhoneOff,
+} from "lucide-react";
+import { FaJava } from "react-icons/fa";
 
 export default function OverallReport() {
-     return (
-          <div className="p-6">
-            <h1 className="text-2xl font-bold mb-6">Overall Report</h1>
-            
-            
+  return (
+    <div className="p-6">
+      <h1 className="text-lg font-bold mb-6">Overall Report</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+        <div className="failed-card ">
+          <div className="failed-card-icon">
+            <Euro size={20} className="icon1" />
           </div>
-        );
+
+          <div className="failed-card-count">
+            <p>2</p>
+          </div>
+
+          <div className="bg-[#f8e8fc] text-center py-2 text-sm text-purple-500 font-medium flex items-center justify-center gap-1 mt-2">
+            Tottal Calls <Info size={12} className="icon1" />
+          </div>
+        </div>
+        <div className="failed-card">
+          <div className="failed-card-icon">
+            <PhoneMissed size={20} className="icon2" />
+          </div>
+
+          <div className="failed-card-count">
+            <p>2</p>
+          </div>
+
+          <div className="bg-[#edf0e3] text-center py-2 text-sm text-green-500 font-medium flex items-center justify-center gap-1 mt-2">
+            Dealed Calls <Info size={12} className="icon2" />
+          </div>
+        </div>
+        <div className="failed-card">
+          <div className="failed-card-icon">
+            <DollarSign size={20} className="icon3" />
+          </div>
+
+          <div className="failed-card-count">
+            <p>.</p>
+          </div>
+
+          <div className="bg-[#f0e4e3] text-center py-2 text-sm text-red-400 font-medium flex items-center justify-center gap-1 mt-2">
+            Failed Calls <Info size={12} className="icon3" />
+          </div>
+        </div>
+        <div className="failed-card">
+          <div className="failed-card-count">
+            <p>.</p>
+          </div>
+
+          <div className="bg-[#ddede6] text-center py-2 text-sm text-green-500 font-medium flex items-center justify-center gap-1 mt-2">
+            Answerd Calls <Info size={12} className="icon4" />
+          </div>
+        </div>
+        <div className="failed-card">
+          <div className="failed-card-icon">
+            <PhoneOff size={20} className="icon5" />
+          </div>
+
+          <div className="failed-card-count">
+            <p>2</p>
+          </div>
+
+          <div className="bg-[#ebd3e5] text-center py-2 text-sm text-pink-600 font-medium flex items-center justify-center gap-1 mt-2">
+            Failed Calls <Info size={12} className="icon5" />
+          </div>
+        </div>
+        <div className="failed-card">
+          <div className="failed-card-icon">
+            <PhoneOff size={20} className="icon6" />
+          </div>
+
+          <div className="failed-card-count">
+            <p>2</p>
+          </div>
+
+          <div className="bg-[#d5ebf0] text-center py-2 text-sm text-sky-500 font-medium flex items-center justify-center gap-1 mt-2">
+            Failed Calls <Info size={12} className="icon6" />
+          </div>
+        </div>
+        <div className="failed-card">
+          <div className="failed-card-icon">
+            <PhoneOff size={20} className="icon7" />
+          </div>
+
+          <div className="failed-card-count">
+            <p>2</p>
+          </div>
+
+          <div className="bg-[#f8e8fc] text-center py-2 text-sm text-purple-500 font-medium flex items-center justify-center gap-1 mt-2">
+            Invalid Calls <Info size={12} className="icon7" />
+          </div>
+        </div>
+        <div className="failed-card">
+          <div className="failed-card-icon">
+            <FaJava size={20} className="icon8" />
+          </div>
+
+          <div className="failed-card-count">
+            <p>2</p>
+          </div>
+
+          <div className="bg-[#f4f3e8] text-center py-2 text-sm text-yellow-500 font-medium flex items-center justify-center gap-1 mt-2">
+            Failed Calls <Info size={12} className="icon8" />
+          </div>
+        </div>
+        <div className="failed-card">
+          <div className="failed-card-count">
+            <p></p>
+          </div>
+
+          <div className="bg-[#e8fafc] text-center py-2 text-sm text-blue-500 font-medium flex items-center justify-center gap-1 mt-2">
+            Follow Ups <Info size={12} className="icon9" />
+          </div>
+        </div>
+        <div className="failed-card">
+          <div className="failed-card-icon">
+            <MessageCircle size={20} className="icon0" />
+          </div>
+
+          <div className="failed-card-count">
+            <p>.</p>
+          </div>
+
+          <div className="bg-[#f5e6f9] text-center py-2 text-sm text-indigo-500 font-medium flex items-center justify-center gap-1 mt-2">
+            Updisposed Calls <Info size={12} className="icon0" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
